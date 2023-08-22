b99f045422866f498cb886e43990cdfc2b17f82b
AndresZunigaZ2005 <andresf.zunigaz@uqvirtual.edu.co>
2023-08-22 10:32:39 -0500

    Metodos CRUD en la branch zz

---
+++ src/co/edu/uniquindio/pr3/exceptions/ClienteException.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.pr3.exceptions;
+
+public class ClienteException extends Exception{
+
+	public ClienteException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClienteException(String mensaje) {
+	      super(mensaje);
+	}
+}
