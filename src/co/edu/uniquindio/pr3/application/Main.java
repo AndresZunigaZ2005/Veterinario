a8ceb9f8ef04aaeed28a2836de01723d84ec0325
AndresZunigaZ2005 <andresf.zunigaz@uqvirtual.edu.co>
2023-08-24 11:18:16 -0500

    Creacion singleton y arreglos menu

--- src/co/edu/uniquindio/pr3/application/Main.java
+++ src/co/edu/uniquindio/pr3/application/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
 
@@ -14,10 +15,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage)   {
 		try {
+	        // Cargar el ícono desde el archivo de recursos
+	        Image icon = new Image(getClass().getResourceAsStream("/imagenes/patita de perro.png"));
+
+	        // Configurar el ícono de la aplicación
+	        primaryStage.getIcons().add(icon);
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/pr3/views/ventanaInicial.fxml"));
 			Parent root = loader.load();
 	        Scene scene = new Scene(root);
 	        primaryStage.setScene(scene);
+	        primaryStage.setResizable(false);
 			ventanaInicialController loginController = loader.getController();
 	        loginController.setStage(primaryStage);
 	        primaryStage.show();
---
+++ src/co/edu/uniquindio/pr3/controllers/ModelFactoryController.java
@@ -0,0 +1,33 @@
+package co.edu.uniquindio.pr3.controllers;
+
+import co.edu.uniquindio.pr3.model.ClinicaVeterinaria;
+import co.edu.uniquindio.pr3.model.Veterinario;
+
+public class ModelFactoryController {
+	ClinicaVeterinaria clinicaVeterinaria = null;
+
+
+
+	//------------------------------  Singleton ------------------------------------------------
+	// Clase estatica oculta. Tan solo se instanciara el singleton una vez
+	private static class SingletonHolder {
+		// El constructor de Singleton puede ser llamado desde aquí al ser protected
+		private final static ModelFactoryController eINSTANCE = new ModelFactoryController();
+	}
+
+	// Método para obtener la instancia de nuestra clase
+	public static ModelFactoryController getInstance() {
+		return SingletonHolder.eINSTANCE;
+	}
+
+	public ModelFactoryController() {
+		System.out.println("invocación clase singleton");
+		inicializarDatos();
+	}
+
+
+	private void inicializarDatos() {
+		clinicaVeterinaria = new ClinicaVeterinaria("Patitas Peluda");
+		Veterinario vet1 = new Veterinario("Luis", "32677297", "luisc.calderonc@uqvirtual.edu.co","vet1");
+	}
+}
